0ae4a91731e42c8c34abe205f9072870a89ed64e
meneses-pt <andremeneses@gmail.com>
2022-10-19 10:34:12 +0100

    Style Adjustments

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,18 +1,18 @@
 import React from "react";
+import "./Nav.css";
 
 function Nav() {
     return <>
         <nav className="navbar navbar-dark bg-dark">
             <a className="navbar-brand" href={"#top"}>
-                <img src="/images/original_icon_w200.png" width="40" height="28" className="d-inline-block align-top"
-                     alt=""/>
-                &nbsp;goals.zone
+                <img src="/react/images/original_icon_w200.png" width="40" height="28" className="d-inline-block align-top"
+                     alt=""/>&nbsp;&nbsp;&nbsp;&nbsp;goals.zone
             </a>
             <div className="form-inline">
                 <button id="moreButton" className="btn btn-success small-btn" data-toggle="modal"
                         data-target="#aboutModal"
                         title="Info">
-                    <img src="/images/circle-info.svg" alt="info" height="15"/>
+                    <img src="/react/images/circle-info.svg" alt="info" height="15"/>
                 </button>
             </div>
         </nav>
