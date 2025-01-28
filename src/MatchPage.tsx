598ec007058c5f4fdbf72405e075e4b76b28abb6
meneses-pt <andremeneses@gmail.com>
2025-01-28 16:49:34 +0000

    Empty

--- src/MatchPage.tsx
+++ src/MatchPage.tsx
@@ -112,9 +112,9 @@ const MatchPage = (props: any) => {
                     <p className="desktop-spacing">&nbsp;{convertToDateTimeStr(match.datetime)}</p>
                 </h3>
             </div>
-            {/*/!* AdsTerra START *!/*/}
-            {/*<div className="ad-container"></div>*/}
-            {/*/!* AdsTerra END *!/*/}
+            {/* 1xbet START */}
+            <div className="ad-container"></div>
+            {/* 1xbet END */}
         </>
     );
 
