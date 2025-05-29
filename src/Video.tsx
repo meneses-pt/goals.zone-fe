3c556f95c7d137dded5b2a0d32856590b2084ef9
meneses-pt <andremeneses@gmail.com>
2025-05-29 13:11:06 +0100

    Open on click if just one mirror

--- src/Video.tsx
+++ src/Video.tsx
@@ -4,11 +4,20 @@ import {Accordion} from "react-bootstrap";
 import "./Video.css";
 
 const Video = ({video}: any) => {
+    const handleHeaderClick = (event: React.MouseEvent) => {
+        if (video.mirrors.length === 1) {
+            event.preventDefault();
+            window.open(video.mirrors[0].url, "_blank");
+        }
+    };
 
     return (
         <>
             <Accordion.Item eventKey={`${video.index}`} className="video">
-                <Accordion.Header data-permalink={video.simple_permalink}>
+                <Accordion.Header
+                    data-permalink={video.simple_permalink}
+                    onClick={handleHeaderClick}
+                >
                     <div id={`video${video.index}`} className="list-group-item no-padding list-group-item-action">
                         <span className="btn btn-link list-group-item-left-link">
                             {video.title}
@@ -19,13 +28,15 @@ const Video = ({video}: any) => {
                         </a>
                     </div>
                 </Accordion.Header>
-                <Accordion.Body>
-                    <ul className="list-group mirror-list">
-                        {video.mirrors.map((m: any, i: number) => {
-                            return <Mirror key={`mirror${video.index}-${i}`} mirror={m}/>;
-                        })}
-                    </ul>
-                </Accordion.Body>
+                {video.mirrors.length > 1 && (
+                    <Accordion.Body>
+                        <ul className="list-group mirror-list">
+                            {video.mirrors.map((m: any, i: number) => {
+                                return <Mirror key={`mirror${video.index}-${i}`} mirror={m}/>;
+                            })}
+                        </ul>
+                    </Accordion.Body>
+                )}
             </Accordion.Item>
         </>
     );
